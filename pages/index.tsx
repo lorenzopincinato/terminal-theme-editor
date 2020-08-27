32d366e75d72921e31bfb5fe865519f58c1203a6
Lorenzo Pincinato <lorenzopincinato@gmail.com>
2020-08-26 21:06:10 -0300

    Change from Create React App to Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      Hello World
+    </div>
+  )
+}
+
+export default Home
