1d322b2d79867b2966eab1e079a73efae1a9d3f0
lorenzopincinato <lorenzopincinato@gmail.com>
2020-08-31 00:30:13 +0000

    Deploying to gh-pages from  @ bbc5b2413719667d0c48c831d13acbb2a7209fdf 🚀

---
+++ _next/static/BjZn8Qd76jYdzrBFG4WWi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-dbc15232ff02ea4a82b2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-83d9a4eab7fa36c1812c.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
