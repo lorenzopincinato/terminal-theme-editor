aa77a5e504b7b236553884940f672f7214973f64
lorenzopincinato <lorenzopincinato@gmail.com>
2020-09-01 00:37:13 +0000

    Deploying to gh-pages from  @ 5440b7dec9fc14388797035461251b69f09c307a 🚀

---
+++ _next/static/3YwwRmcMnZ6K3EGrhvkNj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-b1e87edf5ceeb24c00bb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-848ba791e129c67ff13d.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
